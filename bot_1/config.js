5f5f5167b0aa5ad8f41f419e3d14ddbb785f3a7e
Van Patangan <vanezp01@gmail.com>
2017-06-11 19:32:01 -0700

    Delete config.js

--- bot_1/config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-
-  consumer_key:         'qXFbFqMnN9Kz6g4prqvgnZecl',
-  consumer_secret:      'Zh66x8FqpNB9CJljLPdfsAAkdAFB6oVjQ1y9YDWRkQz5yxbeBF',
-  access_token:         '874054049888206849-sFu4NaCu3gVj4RnteuayfAr0R3OLMh9',
-  access_token_secret:  'iDz9LtuiWTA1zUzFE6Rv478QPwJblmaM3vke4hu46h3MU',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
